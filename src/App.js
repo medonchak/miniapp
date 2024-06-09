e52a8cab2fc22cf80b802b3cf239e7e47e73cdf3
John Doe <archichak999@email.com>
2024-06-09 15:47:11 +0300

    init_two

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 
 function App() {
   const tg = window.Telegram.WebApp
-  const test =  tg.initDataUnsafe.user
+  const test =  tg.initDataUnsafe?.user?.username
   return (
     <div className="App">
     
       
      
-           <button>test</button>
+           <button>{test}</button>
        
         
    
