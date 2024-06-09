231475f873e6139be649e3186a00818d21001178
John Doe <archichak999@email.com>
2024-06-09 20:59:30 +0300

    com1

--- src/component/main_page.jsx
+++ src/component/main_page.jsx
@@ -2,24 +2,22 @@ import React from 'react'
 import { Col, Row } from 'antd';
 const MainPage =(props)=>{
     const mascol =  [
-        <Col style={{background:'silver'}} xs={{span: 5, offset: 1,}} lg={{span: 6, offset: 2,}}>
-            {props.user?.username}
+        <Col style={{background:'silver',width:'120px',height:'120px',margin:'10px'}} xs={{span: 5, offset: 1,}} lg={{span: 6, offset: 2,}}>
+           sdds {props.user?.username}
         </Col>,
-        <Col style={{background:'silver'}} xs={{span: 5, offset: 1,}} lg={{span: 6, offset: 2,}}>
-            {props.user?.username}
+        <Col style={{background:'silver',width:'120px',height:'120px',margin:'10px'}} xs={{span: 5, offset: 1,}} lg={{span: 6, offset: 2,}}>
+         dsds   {props.user?.username}
         </Col>  ]
  
     return (
-        <div>
-            <Row>
+        <div style={{width:'50%',height:'50%'}}>
+            <Row >
                 {mascol}
             </Row>
-            <Row>
+            <Row >
                 {mascol}
             </Row>
-            <Row>
-                {mascol}
-            </Row>
+       
         </div>
     )
 }
