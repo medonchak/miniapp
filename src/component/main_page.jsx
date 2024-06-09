fb9a3a182997fe868e3ef3a85f3d42ef72845ff8
John Doe <archichak999@email.com>
2024-06-09 21:05:43 +0300

    com3

--- src/component/main_page.jsx
+++ src/component/main_page.jsx
@@ -6,18 +6,18 @@ const MainPage =(props)=>{
     return (
         <div >
             <Row >
-                <Col style={{background:'silver',width:'120px',height:'120px',margin:'10px'}} xs={{span: 5, offset: 1,}} lg={{span: 6, offset: 2,}}>
+                <Col style={{background:'silver',width:'100%',height:'120px',margin:'10px'}} xs={{span: 5, offset: 1,}} lg={{span: 6, offset: 2,}}>
                     Інвестиції  
                 </Col>  
-                <Col style={{background:'silver',width:'120px',height:'120px',margin:'10px'}} xs={{span: 5, offset: 1,}} lg={{span: 6, offset: 2,}}>
+                <Col style={{background:'silver',width:'100%',height:'120px',margin:'10px'}} xs={{span: 5, offset: 1,}} lg={{span: 6, offset: 2,}}>
                     Дропи  
                 </Col>  
             </Row>
             <Row >
-                <Col style={{background:'silver',width:'120px',height:'120px',margin:'10px'}} xs={{span: 5, offset: 1,}} lg={{span: 6, offset: 2,}}>
+                <Col style={{background:'silver',width:'100%',height:'120px',margin:'10px'}} xs={{span: 5, offset: 1,}} lg={{span: 6, offset: 2,}}>
                     Тестнет   
                 </Col>  
-                <Col style={{background:'silver',width:'120px',height:'120px',margin:'10px'}} xs={{span: 5, offset: 1,}} lg={{span: 6, offset: 2,}}>
+                <Col style={{background:'silver',width:'100%',height:'120px',margin:'10px'}} xs={{span: 5, offset: 1,}} lg={{span: 6, offset: 2,}}>
                     Смарт ідеї  
                 </Col> 
             </Row>
